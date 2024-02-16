5b5e59c08e0ff2c30e86b9d9054ba71f1930bafe
Mohamed Gouda <goda61@gmail.com>
2024-02-16 22:09:30 +0200

    Fix some problems in view

--- blog/views.py
+++ blog/views.py
@@ -36,38 +36,3 @@ def post_detail (request,slug) :
     }
     return render (request , 'posts/post_detail.html' , context)
 
-
-
-# def create_post(request):
-#     if request.method == 'POST' :
-#         form = PostForm(request.POST,request.FILES)
-#         if form.is_valid() :
-#             myform = form.save(commit=False)
-#             myform.author = request.user
-#             myform.save()
-#             return redirect ('/posts/')
-#     else :
-#         form = PostForm()
-
-#     return render (request,'posts/post_form.html',{'form':form})
-
-# def edit_post(request,pk):
-#     post = Post.objects.get(id=pk)
-
-#     if request.method == 'POST' :
-#         form = PostForm(request.POST,request.FILES,instance=post)
-#         if form.is_valid() :
-#             myform = form.save(commit=False)
-#             myform.author = request.user
-#             myform.save()
-#             return redirect ('/posts/')
-#     else :
-#         form = PostForm(instance=post)
-
-#     return render (request,'posts/edit.html',{'form':form})
-
-
-# def delete_post(request,pk):
-#     post = Post.objects.get(id=pk)
-#     post.delete()
-#     return redirect ('/posts/')
